47fb981f81d27d360a8b8e9e15528626d814e82b
nikolasimeunovic <sime97kv@gmail.com>
2018-03-18 14:00:59 +0100

    Dodate hashCode, equals i toString metode

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -45,4 +45,56 @@ public class Valuta {
 	public void setDatum(GregorianCalendar datum) {
 		this.datum = datum;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ",datum: " + datum + ", prodajni kurs: "
+						+ prodajni + ", kupovni kurs=" + kupovni + ", srednji kurs=" + srednji + "]";
+	}
 }
