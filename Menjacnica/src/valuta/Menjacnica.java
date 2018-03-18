8f91c846c67874d19c858779b99426344c6623f3
nikolasimeunovic <sime97kv@gmail.com>
2018-03-18 14:20:12 +0100

    Napravljena klasa Menjacnica

---
+++ Menjacnica/src/valuta/Menjacnica.java
@@ -0,0 +1,27 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+import interfejsi.menjacnicaInterfejs;
+
+public class Menjacnica implements menjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean obrisiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Valuta pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
