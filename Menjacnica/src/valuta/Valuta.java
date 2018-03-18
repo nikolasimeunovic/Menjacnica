2f18e706dfd82c3405bdd824e216a03c968a4c64
nikolasimeunovic <sime97kv@gmail.com>
2018-03-18 13:56:12 +0100

    Uvedena klasa Valuta sa get i set metodama

---
+++ Menjacnica/src/valuta/Valuta.java
@@ -0,0 +1,48 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+
+	private String naziv, skraceniNaziv;
+	private double prodajni,srednji,kupovni;
+	private GregorianCalendar datum;
+	
+
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+}
